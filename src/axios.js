e01ec7a25f179fc78ce63e9887c3b720c88e66ad
chirag142452 <chi.chiragbhardwaj@gmail.com>
2023-08-24 20:24:29 +0530

    changes

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const InstanceAxios = () =>{
     const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "https://contactoappbackend-production.up.railway.app/",
+    baseURL: "https://contactoappbackend-production.up.railway.app",
     headers: {
       'Content-Type': 'application/json',
        Authorization : "bearer "+token,
